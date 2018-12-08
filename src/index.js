ddb0cd1c03fe8d939bf5ddddac08602c0a602f8d
Alexey Modin <alexey@brace.ai>
2018-12-08 16:29:04 +0300

    Initial

---
+++ src/index.js
@@ -0,0 +1,114 @@
+import React, { Component, Fragment } from "react";
+import classnames from "classnames";
+import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
+
+class ChainedModalComponent extends Component {
+  constructor(props) {
+    super();
+    this.container = null;
+    const { elements } = props
+    this.state = {
+      visible: props.visible || true,
+      currentModal: 0,
+      totalModals: elements.length || 0,
+      animationNext: props.animationNext || "fadeInRight",
+      animationPrev: props.animationPrev || "fadeInLeft",
+      animation: props.animationNext || "fadeInRight"
+    };
+  }
+
+  showNext() {
+    if (this.state.currentModal < this.state.totalModals - 1) {
+      this.setState({
+        animation: this.props.animationNext || "fadeInRight",
+        currentModal: this.state.currentModal + 1
+      });
+    }
+  }
+
+  showPrev() {
+    if (this.state.currentModal > 0) {
+      this.setState({
+        animation: this.props.animationPrev || "fadeInLeft",
+        currentModal: this.state.currentModal - 1
+      });
+    }
+  }
+
+  returnWrappedElement(El, props, index) {
+    if (this.state.currentModal === index)
+      return (
+        <div
+          key={index}
+          className={classnames(
+            "react-chained-modal-container animated",
+            this.state.animation,
+            this.props.animationSpeed
+          )}
+        >
+          <El
+            closeChainedModal={this.closeModal}
+            className="wrapped-element"
+            currentModal={this.state.currentModal}
+            totalModals={this.state.totalModals}
+            showNext={() => this.showNext.bind(this)}
+            showPrev={() => this.showPrev.bind(this)}
+            {...props}
+          />
+        </div>
+      );
+  }
+
+  render() {
+    return (
+      <Fragment>
+        {(this.props.visible &&
+          ReactDOM.createPortal(
+            <div
+              className="react-chained-modal-cover"
+              id="react-chained-modal-portal"
+              onClick={e => {
+                if (
+                  e.target.id === "react-chained-modal-portal" &&
+                  this.props.closeOnBackground
+                ) {
+                  this.props.onRequestClose();
+                }
+              }}
+            >
+              {this.props.elements.length &&
+                this.props.elements.map((item, index) => {
+                  return this.returnWrappedElement(
+                    item.component,
+                    item.props,
+                    index
+                  );
+                })}
+            </div>,
+            document.body
+          )) ||
+          null}
+      </Fragment>
+    );
+  }
+}
+
+const ChainedModal = props => {
+  if (props.visible) {
+    return <ChainedModalComponent {...props} />;
+  } else {
+    return null;
+  }
+};
+
+ChainedModalComponent.propTypes = {
+  visible: PropTypes.bool.isRequired,
+  elements: PropTypes.array.isRequired,
+  animationNext: PropTypes.string,
+  animationPrev: PropTypes.string,
+  closeOnBackground: PropTypes.bool,
+  onRequestClose: PropTypes.func.isRequired
+};
+
+export default ChainedModal;
